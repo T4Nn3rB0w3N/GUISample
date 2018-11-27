a6b523c155f8f952c06664903624f14dca276178
Bowen <tbow1382@740s-pl-120.local>
2018-11-27 08:16:38 -0700

    Frame is complete
    data members (the controller and the panel) are declared, then initialized in the constructor (with the controller as a parameter).  the helper method, setupFrame, is created.

--- src/sample/view/SampleFrame.java
+++ src/sample/view/SampleFrame.java
@@ -5,6 +5,25 @@ import sample.controller.GUIController;
 
 public class SampleFrame extends JFrame
 {
+	private GUIController appController;
+	private SamplePanel appPanel;
 	
-
+	public SampleFrame(GUIController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		this.appPanel = new SamplePanel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(1000, 500);
+		this.setTitle("Second GUI App");
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
