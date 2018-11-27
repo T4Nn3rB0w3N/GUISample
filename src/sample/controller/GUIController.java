518d3f37f04025ca54d9b4f1b142655fc0f2afb8
Bowen <tbow1382@740s-pl-120.local>
2018-11-27 08:28:36 -0700

    imported SampleFrame, declared SampleFrame as data member, and created contructor that intializes the data member.

--- src/sample/controller/GUIController.java
+++ src/sample/controller/GUIController.java
@@ -1,11 +1,21 @@
 package sample.controller;
 
+import sample.view.SampleFrame;
+
 public class GUIController
 {
+	private SampleFrame appFrame;
+	
+	public GUIController()
+	{
+		this.appFrame = new SampleFrame(this);
+		
+	}
 	
 	public void start()
 	{
 		
 	}
+	
 
 }
